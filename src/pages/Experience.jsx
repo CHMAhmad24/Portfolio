2723927d919aa685ff92dc2dbdc78b09c3826a5b
Ch Muhammad Ahmad <chmahmad24@gmail.com>
2026-02-23 19:06:23 +0500

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -83,7 +83,7 @@ export default function Experience() {
   const lineSize = useTransform(scrollYProgress, (v) => `${v * 100}%`)
 
   return (
-    <section id='experience' className='relative bg-black text-white h-full'>
+    <section id='experience' className='relative bg-black text-white h-[200vh]'>
       <div ref={sceneRef} style={{ height: `${scene_height_VH}vh`, minHeight: "120vh" }} className='relative'>
         <div className='sticky top-0 h-screen flex flex-col'>
           <h2 className='text-4xl sm:text-5xl font-semibold mt-5 text-center'>Experience</h2>
